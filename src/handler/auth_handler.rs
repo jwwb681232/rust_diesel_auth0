4f6c4f3ed2aa7c7931b9057ec3ee5bacd5634cfc
wangxiao <wx497657341@gmail.com>
2020-09-06 23:31:20 +0800

    Select from enum when the handler returns an error, and then error_response returns the corresponding information according to enum, and you can also add the status_code method

--- src/handler/auth_handler.rs
+++ src/handler/auth_handler.rs
@@ -1,18 +1,43 @@
-use actix_web::{web, HttpResponse};
+use std::{fmt::{Display,Formatter,Result as fmtResult}};
 use serde::{Serialize,Deserialize};
+use actix_web::{ResponseError, HttpResponse};
+use actix_web::http::StatusCode;
 use serde_json::json;
-use crate::error::ServiceError;
 
-#[derive(Serialize, Deserialize)]
-pub struct RegisterFrom {
-    pub name: String,
-    pub email: String,
-    pub password: String,
+#[derive(Debug,Deserialize,Serialize)]
+pub struct ApiError{
+    pub status_code:u16,
+    pub message: String,
 }
 
-pub async fn index(_form: web::Form<RegisterFrom>) -> Result<HttpResponse,ServiceError> {
-    Ok(
-        HttpResponse::Ok().json("")
-    )
-    //Err(ServiceError::new(StatusCode::NOT_FOUND, "NotFound".to_string()))
+impl ApiError{
+    pub fn new(status_code:u16,message:String) ->ApiError {
+        ApiError{ status_code, message }
+    }
+}
+
+impl Display for ApiError{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmtResult {
+        write!(f,"{}",self.message.as_str())
+    }
+}
+
+impl ResponseError for ApiError{
+    fn error_response(&self)->HttpResponse{
+        let status_code = match StatusCode::from_u16(self.status_code) {
+            Ok(code) => code,
+            Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        let message = match status_code.as_u16() < 500 {
+            true => self.message.clone(),
+            false=> "Internal Server Error".to_string()
+        };
+
+        HttpResponse::build(status_code).json(json!({ "message": message }))
+    }
+}
+
+pub async fn index() -> actix_web::Result<String, ApiError> {
+    Err(ApiError::new(500,"NotFound".to_string()))
 }
