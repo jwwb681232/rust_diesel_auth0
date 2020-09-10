d23c0dc70c8cb5dda2dcf7279eb26dd98875ef3f
wangxiao <wx497657341@gmail.com>
2020-09-10 23:15:33 +0800

    fix route scope

--- src/route/user_route.rs
+++ src/route/user_route.rs
@@ -5,12 +5,12 @@ use crate::handler::auth_handler::InputUser;
 
 pub fn route(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/")
-            .route("users", web::get().to(auth_handler::get_users))
+        web::scope("/users")
+            .route("", web::get().to(auth_handler::get_users))
             .route("{id}", web::get().to(auth_handler::get_user_by_id))
             .route("{id}", web::delete().to(auth_handler::delete_user))
             .service(
-                web::resource("users")
+                web::resource("")
                     .app_data(web::Json::<InputUser>::configure(|json_config| {
                         json_config.error_handler(json_error_handler)
                     }))
