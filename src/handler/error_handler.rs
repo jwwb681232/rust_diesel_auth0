6827f3c77a530fcf920f8cbc44881638a929781e
wangxiao <wx497657341@gmail.com>
2020-09-10 22:15:21 +0800

    catch json error

--- src/handler/error_handler.rs
+++ src/handler/error_handler.rs
@@ -6,12 +6,12 @@ use crate::rim::error::ApiError;
 
 pub fn json_error_handler(err: JsonPayloadError, _req: &HttpRequest) -> Error {
     let response = match &err {
-        JsonPayloadError::Deserialize(e) => {
-            HttpResponse::Ok().json(ApiError{ status_code: 200, message: Box::leak(e.to_string().into_boxed_str()) })
-        }
-        _ => {
-            HttpResponse::UnsupportedMediaType().content_type("plan/text").body(err.to_string())
-        }
+        JsonPayloadError::Deserialize(e) =>
+            HttpResponse::Ok().json(ApiError { status_code: 200, message: Box::leak(e.to_string().into_boxed_str()) }),
+        JsonPayloadError::Payload(e) =>
+            HttpResponse::Ok().json(ApiError { status_code: 200, message: Box::leak(e.to_string().into_boxed_str()) }),
+        JsonPayloadError::ContentType | JsonPayloadError::Overflow =>
+            HttpResponse::Ok().json(ApiError { status_code: 200, message: Box::leak(err.to_string().into_boxed_str()) }),
     };
 
     //ApiError::new()
